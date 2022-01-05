89d0c4c65f92e2664e0480120e3a204c8a156e82
Yevhenii Ibrahimov <yevhenii.ibrahimov@globallogic.com>
2022-01-05 15:55:51 +0200

    Add project files.

---
+++ TestTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
